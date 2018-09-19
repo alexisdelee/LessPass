bb53600346a502d87b18e8aa537a4f92c441a204
Alexis Delee <alexisdelee@protonmail.com>
2018-09-19 22:00:23 +0200

    documentation for algorithms

--- algorithm.sh
+++ algorithm.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 declare -A algorithms=( # support hash algorithms
-    [sha1]="sha1sum"
-    [sha256]="sha256sum"
-    [sha384]="sha384sum"
-    [sha512]="sha512sum"
-    [md5]="md5sum"
+    [sha1]="sha1sum" # max 41 characters
+    [sha256]="sha256sum" # max 65 characters
+    [sha384]="sha384sum" # max 97 characters
+    [sha512]="sha512sum" # max 129 characters
+    [md5]="md5sum" # max 33 characters
 )
 
 algorithm()
